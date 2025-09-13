4d33410677d790eca02fe7c68656b5bc29bf507f
apathyjade <apathyjade@outlook.com>
2025-09-13 22:06:17 +0800

    build: 添加 RISC-V 架构支持并扩展 Linux 兼容性
    - 在 Makefile.toml 中添加了针对 riscv64 架构的构建任务
    - 更新 main.rs 文件，增加对 Linux 系统的支持

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 
-#![cfg(any(target_os = "windows", target_os = "macos"))]
+#![cfg(any(target_os = "windows", target_os = "macos", target_os = "linux"))]
 #![windows_subsystem = "windows"]
 
 mod ui;
